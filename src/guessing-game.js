e131abcbd33aa9f3190804fdc6832c81a9898487
Yury Maniakhin <manah9191@mail.ru>
2021-08-12 12:27:38 +0300

    feat: complite task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        let min = 0;
+        let max = 0;
+        let average = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.average = Math.ceil((this.min + this.max) / 2)
+        return this.average
     }
 
     lower() {
-
+        if (this.average < this.max) {
+            return this.max = this.average;
+        }
     }
 
     greater() {
-
+        if (this.average < this.max) {
+            return this.min = this.average;
+        }
     }
 }
 
